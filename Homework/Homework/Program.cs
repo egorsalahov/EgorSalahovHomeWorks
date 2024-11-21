426587ebbc7546e55f23b92d14af8cbc668f0beb
EgorSalahov <egorsalahov2@gmail.com>
2024-11-21 18:54:52 +0300

    dfghj

---
+++ Homework/Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿using Homework._1_задача;
+using System.Text.RegularExpressions;
+
+namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
