c6176a08815e9f11f54ae0878e13a7c2366a9590
EgorSalahov <egorsalahov2@gmail.com>
2025-06-04 11:04:41 +0300

    #2 async/await hw

---
+++ Homework/Homework/AsyncAwait/DeadLockResultClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.AsyncAwait
+{
+    internal class DeadLockResultClass
+    {
+        public static void DefalutMethod()
+        {
+            int result = AnotherMethodAsync().Result;
+        }
+
+        public static async Task<int> AnotherMethodAsync()
+        {
+            Console.WriteLine("Я в async методе");
+            await Task.Delay(1000); //deadlock
+
+            return 0;
+        }
+    }
+}
